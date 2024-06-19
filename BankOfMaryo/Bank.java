e5a5509dbdeca7055dcd105c1af17ffab81d3e89
LordMaryo <emmanuelofoneta@gmail.com>
2024-06-20 04:10:14 +0630

    Authenticate username

--- BankOfMaryo/Bank.java
+++ BankOfMaryo/Bank.java
@@ -12,20 +12,30 @@ public class Bank {
 
     Random random = new Random();
     int pin = 1000 + random.nextInt(9000);
-    int usersPin;
+    int inputPin;
 
     public void createAccount() {
         System.out.print("Enter account holder's name: ");
-        this.userName = input.nextLine();
+        String userInput = input.nextLine();
+        boolean isUserInputBlank = userInput == null || userInput.trim().isEmpty();
 
-        System.out.println("Welcome " + this.userName + ". Your 4-digit PIN is: " + pin);
+        // TODO: make sure correct User name is stored after second try
+
+        if (isUserInputBlank) {
+            System.out.println("User name cannot be blank.");
+            System.out.print("Enter account holder's name: ");
+            userInput = input.nextLine();
+        } else {
+            this.userName = userInput;
+            System.out.println("Welcome " + this.userName + ". Your 4-digit PIN is: " + pin);
+        }
     }
 
     public void depositMoney() {
         System.out.print("Enter pin to deposit: ");
-        usersPin = input.nextInt();
+        inputPin = input.nextInt();
 
-        if (usersPin == pin) {
+        if (inputPin == pin) {
             initiateDeposit();
         } else {
             checkAuthentication();
@@ -43,8 +53,8 @@ public class Bank {
         while (tries > 1) {
             System.out.println("Incorrect Pin. you have " + (tries - 1) + " more tries");
             System.out.print("Enter pin to deposit: ");
-            usersPin = input.nextInt();
-            if (usersPin == pin) {
+            inputPin = input.nextInt();
+            if (inputPin == pin) {
                 initiateDeposit();
                 return;
             }
